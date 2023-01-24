fd9660deafb69c9723191144a46844b046beb2d4
akhilsha <akhil.sharma3@amdocs.com>
2023-01-24 10:15:14 +0530

    correcting linting errors

--- app/app/calc_func.py
+++ app/app/calc_func.py
@@ -6,4 +6,3 @@ Calculate sun of two numbers
 def sum(x, y):
     """return sum of x, y"""
     return x + y
-
--- app/app/test.py
+++ app/app/test.py
@@ -6,10 +6,11 @@ from django.test import SimpleTestCase
 
 from app import calc_func
 
+
 class CalcTest(SimpleTestCase):
     """test calc omdule"""
 
-    def  test_add_numbers(self):
+    def test_add_numbers(self):
         """test adding numbers together"""
 
         res = calc_func.sum(5, 6)
