451e6e0f860f55905555d705ebb3c5987a76286b
HRUAS <akhil@localhost.localdomain>
2023-01-24 00:25:16 -0500

    commit from hruas

--- app/app/test.py
+++ app/app/test.py
@@ -12,7 +12,5 @@ class CalcTest(SimpleTestCase):
 
     def test_add_numbers(self):
         """test adding numbers together"""
-
         res = calc_func.sum(5, 6)
-
         self.assertEqual(res, 11)
