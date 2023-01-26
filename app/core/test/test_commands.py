a5e8406e97f3eb5213726f8666cc1506b9262ace
akhilsha <akhil.sharma3@amdocs.com>
2023-01-26 11:40:13 +0530

    added test module for database connection - 2

--- app/core/test/test_commands.py
+++ app/core/test/test_commands.py
@@ -12,7 +12,7 @@ from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
-@patch('core.management.cpmmands.wait_for_db.Commands.check')
+@patch('core.management.commands.wait_for_db.Commands.check')
 class CommandTests(SimpleTestCase):
     """Test commands"""
 
