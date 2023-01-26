7ece4f00e5db7a2bbc04fec55b38ebb1662b4521
akhilsha <akhil.sharma3@amdocs.com>
2023-01-26 12:10:53 +0530

    connecting database 4

--- app/core/test/test_commands.py
+++ app/core/test/test_commands.py
@@ -21,7 +21,6 @@ class CommandTests(SimpleTestCase):
         patched_check.return_value = True
 
         call_command('wait_for_db')
-        call_command('wait_for_db')
 
         patched_check.assert_called_once_with(databases=['default'])
 
