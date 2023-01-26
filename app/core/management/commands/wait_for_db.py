a7a0010ee90ea9ddbe5f7ed8c23e32a49b75597c
akhilsha <akhil.sharma3@amdocs.com>
2023-01-26 12:32:50 +0530

    connecting database 11

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -21,6 +21,6 @@ class Command(BaseCommand):
                 db_up = True
             except Exception as exp:
                 self.stdout.write('Database unavailable waiting 1 second....')
-                self.stdout.write(self.style.ERROR(f'Exception : {exp}'))
+                print(f'Exception : {exp}')
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available'))
