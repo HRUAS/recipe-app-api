dba4449a135d2c2edc7e15ab7f700d32eeef880d
akhilsha <akhil.sharma3@amdocs.com>
2023-01-26 12:30:23 +0530

    connecting database 10

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,9 +2,9 @@
 Django commands to wait for the database to be available
 """
 import time
-from psycopg2 import OperationalError as Psycopg2Error
+# from psycopg2 import OperationalError as Psycopg2Error
 from django.core.management.base import BaseCommand
-from django.db.utils import OperationalError
+# from django.db.utils import OperationalError
 
 
 class Command(BaseCommand):
