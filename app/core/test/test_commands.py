561ee15228a73c43e224c821d244c3be7a7f098d
akhilsha <akhil.sharma3@amdocs.com>
2023-01-26 12:18:06 +0530

    connecting database 6

--- app/core/test/test_commands.py
+++ app/core/test/test_commands.py
@@ -16,16 +16,6 @@ from django.test import SimpleTestCase
 class CommandTests(SimpleTestCase):
     """Test commands"""
 
-    def test_wait_for_db_ready(self, patched_check):
-        print("starting test_wait_for_db_ready")
-        """Test wait for database to be ready"""
-        patched_check.return_value = True
-
-        call_command('wait_for_db')
-
-        patched_check.assert_called_once_with(databases=['default'])
-        print("ending test_wait_for_db_ready")
-
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         print("starting test_wait_for_db_delay")
@@ -40,3 +30,13 @@ class CommandTests(SimpleTestCase):
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
         print("starting test_wait_for_db_delay")
+
+    def test_wait_for_db_ready(self, patched_check):
+        print("starting test_wait_for_db_ready")
+        """Test wait for database to be ready"""
+        patched_check.return_value = True
+
+        call_command('wait_for_db')
+
+        patched_check.assert_called_once_with(databases=['default'])
+        print("ending test_wait_for_db_ready")
