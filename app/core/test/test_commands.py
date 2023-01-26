e1b8c5f9f2c6b4afb0526451bca0be796c242ed1
akhilsha <akhil.sharma3@amdocs.com>
2023-01-26 12:14:34 +0530

    connecting database 5

--- app/core/test/test_commands.py
+++ app/core/test/test_commands.py
@@ -17,15 +17,19 @@ class CommandTests(SimpleTestCase):
     """Test commands"""
 
     def test_wait_for_db_ready(self, patched_check):
+        print("starting test_wait_for_db_ready")
         """Test wait for database to be ready"""
         patched_check.return_value = True
 
         call_command('wait_for_db')
 
         patched_check.assert_called_once_with(databases=['default'])
+        print("ending test_wait_for_db_ready")
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        print("starting test_wait_for_db_delay")
+
         """Test waiting for database getting OperationalError"""
 
         patched_check.side_effect = [Psycopg2Error] * 2 + \
@@ -35,3 +39,4 @@ class CommandTests(SimpleTestCase):
 
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
+        print("starting test_wait_for_db_delay")
