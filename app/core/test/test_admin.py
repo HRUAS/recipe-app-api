f6a016cd5f23e6a7a45df053b36bd55aa41bbb72
akhilsha <akhilsharma433@gmail.com>
2023-01-28 19:20:08 +0530

    added test_user_list

---
+++ app/core/test/test_admin.py
@@ -0,0 +1,36 @@
+"""
+Tests for django admin modifications
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+
+class AdminSiteTests(TestCase):
+    """Tests for Django admin"""
+
+    def setUp(self):
+        """create user and client"""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='1234',
+        )
+
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='1234',
+            name='user'
+        )
+
+    def test_user_list(self):
+        """Test if user are listed on the page"""
+        print("Starting test_user_list")
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
