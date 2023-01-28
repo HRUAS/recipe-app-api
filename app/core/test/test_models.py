5f314abceb297da2b9e1744ed783a0fb2ba62f12
akhilsha <akhilsharma433@gmail.com>
2023-01-28 18:53:30 +0530

    correcting test_new_user_without_email_raises_error

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -37,5 +37,6 @@ class ModelTests(TestCase):
 
     def test_new_user_without_email_raises_error(self):
         """Tests if that tries to create a user without email"""
+        print("Starting test_new_user_without_email_raises_error")
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'test123')
