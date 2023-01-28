bfa593249da31a0cebe18aaf15adf42986a8cb4a
akhilsha <akhilsharma433@gmail.com>
2023-01-28 19:47:22 +0530

    corrected test_create_user_page

--- app/core/test/test_admin.py
+++ app/core/test/test_admin.py
@@ -46,7 +46,7 @@ class AdminSiteTests(TestCase):
     def test_create_user_page(self):
         """Tests to check user page works"""
         print("Staring test_create_user_page")
-        url = reverse('admin:core_user_change')
+        url = reverse('admin:core_user_add')
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
