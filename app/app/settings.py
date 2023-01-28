5a39b92a8b48e021bfcd83f1932071730e71106e
akhilsha <akhilsharma433@gmail.com>
2023-01-28 18:19:12 +0530

    correcting SECRET_KEY

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uk=cj%zh@ob0%s(gmjas=5)5vzy-8)'
-+ '^$tnn0)#93(=xv4ze2_9'
+SECRET_KEY = 'django-insecure-uk=cj%zh@ob0%s(gmjas=5)5vzy-8)\
+    ^$tnn0)#93(=xv4ze2_9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
