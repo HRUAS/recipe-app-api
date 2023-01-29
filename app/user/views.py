ef70ecb0c3ba9af44f971dd3bafdbb8ccc6de0be
akhilsha <akhilsharma433@gmail.com>
2023-01-29 17:11:11 +0530

    func: correcting typo in UserSerializers

--- app/user/views.py
+++ app/user/views.py
@@ -4,9 +4,9 @@ Views for the user API
 
 from rest_framework import generics
 
-from user.serializers import UserSerializer
+from user.serializers import UserSerializers
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
-    serializer_class = UserSerializer
+    serializer_class = UserSerializers
