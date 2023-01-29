03f8f920762d6facdec8118b309aedfa3ce01a01
akhilsha <akhilsharma433@gmail.com>
2023-01-29 17:15:41 +0530

    correcting post typo in test

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -59,7 +59,7 @@ class PublicUserApiTests(TestCase):
             'name': 'test'
         }
 
-        res = self.client.port(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
