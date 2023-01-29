00d6d1578e5a2e440a8f63b0a786848c32e7177e
akhilsha <akhilsharma433@gmail.com>
2023-01-29 18:40:34 +0530

    func: added tokenization

--- app/user/views.py
+++ app/user/views.py
@@ -3,10 +3,20 @@ Views for the user API
 """
 
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializers
+from user.serializers import (
+    UserSerializers, AuthTokenSerializer,
+)
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
     serializer_class = UserSerializers
+
+
+class CreateTokenview(ObtainAuthToken):
+    """Create a new auth token for user"""
+    serializer_class = AuthTokenSerializer
+    render_classes = api_settings.DEFAULT_RENDERER_CLASSES
