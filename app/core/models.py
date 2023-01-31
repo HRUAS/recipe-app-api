e85765633415917744c19ec644e3cc6e11c0c19b
akhilsha <akhilsharma433@gmail.com>
2023-01-31 07:49:25 +0530

    func: added Recipe class - 4

--- app/core/models.py
+++ app/core/models.py
@@ -48,7 +48,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 class Recipe(models.Model):
     """Recipe Object"""
-    users = models.ForeignKey(
+    user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
     )
